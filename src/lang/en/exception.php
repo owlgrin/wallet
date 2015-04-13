6543beb2738b8ed2284ee9c72a9e0b87e750d9c0
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-04-13 09:40:29 +0000

    fixed the language file when credit limit exhausts

--- src/lang/en/exception.php
+++ src/lang/en/exception.php
@@ -4,7 +4,7 @@ return [
 	'message' => [
 		'bad_request'   			=> 'Request cannot be processed.',
 		'internal_error'			=> 'Something went wrong. It\'s our mistake and we are fixing it',
-		'credits_limit_reached'		=> 'Credits limit has been reached.',
+		'credits_limit_reached'		=> 'No more credits can be applied.',
 		'no_credits'				=> 'You dont have any credits.'
 	]
 ];
